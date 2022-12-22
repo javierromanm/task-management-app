19d9bccb60a6e1ebcc49a8ebe455a3f730773cf9
Javier Roman Minguez <javierroman@javierroman.com>
2022-12-22 13:43:41 +0100

    Include a form to add a new assignment

--- public/js/components/Assignments.js
+++ public/js/components/Assignments.js
@@ -4,7 +4,13 @@ export default {
     template: `
         <section class="space-y-6">
             <assignment-list :assignments="filters.inProgress" title="In Progress"></assignment-list>
-            <assignment-list :assignments="filters.completed" title="Completed"></assignment-list>        
+            <assignment-list :assignments="filters.completed" title="Completed"></assignment-list>      
+            <form @submit.prevent="add">
+                <div class="text-black border border-gray-600">
+                    <input v-model="newAssignment" type="text" placeholder="New assignment" class="p-2">
+                    <button type="submit" class="bg-white p-2 border-l">Add</button>
+                </div>
+            </form>  
         </section>
     `,
     data() {
@@ -13,7 +19,8 @@ export default {
                 { name: 'Task 1', completed: false, id: 1},
                 { name: 'Task 2', completed: false, id: 2},
                 { name: 'Task 3', completed: false, id: 3}
-            ]
+            ],
+            newAssignment: ''
         }
     },
     computed: {        
@@ -23,5 +30,15 @@ export default {
                 completed: this.assignments.filter(a => a.completed)
             }            
         }
-    } 
+    },
+    methods: {
+        add() {
+            this.assignments.push({
+                name: this.newAssignment,
+                completed: false,
+                id: this.assignments.length + 1
+            })
+            this.newAssignment = ''
+        }
+    }
 }
